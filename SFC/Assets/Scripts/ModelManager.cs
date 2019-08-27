e9141f94a264f3594ef48f4a6495e060f87f37f7
rishi4rks <rishikeshshede@gmail.com>
2019-08-27 09:49:55 +0530

    AR app made for SFC

---
+++ SFC/Assets/Scripts/ModelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelManager : MonoBehaviour {
+
+    public GameObject[] Models;
+    private void Start()
+    {
+        //Disable all models at start
+        DisableAll();
+    }
+    void DisableAll()
+    {
+        foreach (var item in Models)
+        {
+            item.SetActive(false);
+        }
+    }
+    void SetModel(int index)
+    {
+        //Disable all other models before enabling a model
+        DisableAll();
+        Models[index].SetActive(true);
+    }
+}
---
+++ SFC/Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public GameObject ModelsManager;
+    public Sprite[] SelectedSprites;
+    public Sprite[] NormalSprites;
+    public Image[] SliderImages;
+    // Use this for initialization
+    void Start () {
+        DeselectAll();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SelectModel(int index)
+    {
+        ModelsManager.SendMessage("SetModel", (index));
+        //Set all images to be deselected
+        DeselectAll();
+        SliderImages[index].sprite = SelectedSprites[index];
+    }
+
+    void DeselectAll()
+    {
+        for (int i = 0; i < SliderImages.Length; i++)
+        {
+            SliderImages[i].sprite = NormalSprites[i];
+        }
+    }
+}
